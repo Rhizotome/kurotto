515334cec176c9e3058c3f299ffdec3a0ec15d39
Rhizotome <dam.elalami@gmail.com>
2020-04-24 21:55:42 +0200

    on essaie sparsepp

--- formule.hpp
+++ formule.hpp
@@ -6,9 +6,11 @@
 #include <unordered_set>
 #include <set>
 #include <mutex>
+#include "sparsepp/sparsepp/spp.h"
+
 using litt = char;
 template <class T>
-using CInt = std::unordered_set<T>;
+using CInt = spp::sparse_hash_set<T>;
 template <class T>
 using CExt = std::vector<T>;
 
